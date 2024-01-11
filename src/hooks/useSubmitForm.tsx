4b34f57f58d3b11523f3aa52a43982639f7f99f8
unknown <alfesen>
2024-01-11 13:41:44 +0100

    Add hook for posting

---
+++ src/hooks/useSubmitForm.tsx
@@ -0,0 +1,30 @@
+import toast from 'react-hot-toast';
+import { IErrorResponse } from '../types';
+
+const useSubmitForm = () => {
+  const submitHandler = async (url: string, data: FormData | Record<string, unknown>) => {
+    const isFormData = data instanceof FormData;
+
+    const body = isFormData ? data : JSON.stringify(data);
+
+    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}${url}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: !isFormData ? {
+        'Content-Type': 'application/json',
+      } : undefined,
+      body,
+    });
+
+    if (!response.ok) {
+      const res = await response.json() as IErrorResponse;
+      return toast.error(res.message);
+    } 
+    
+    toast.success('Submission successful');
+  };
+
+  return submitHandler;
+};
+
+export default useSubmitForm;
