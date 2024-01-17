2a3af10bda5b186c21e1989152ba2202583a3a92
unknown <alfesen>
2024-01-17 15:54:55 +0100

    Implement Tech updating logic + list

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,28 @@
-import { Container, Divider, Stack } from '@mui/material'
-import TechForm from './components/forms/TechForm'
-import PromotionForm from './components/forms/PromotionForm'
-import LoginForm from './components/forms/LoginForm'
-import { Toaster } from 'react-hot-toast'
+import { Container, Divider, Stack } from "@mui/material";
+import TechForm from "./components/forms/TechForm";
+import PromotionForm from "./components/forms/PromotionForm";
+import LoginForm from "./components/forms/LoginForm";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "react-query";
+import TechList from "./components/lists/TechList";
 
 export function App() {
+  const client = new QueryClient();
   return (
     <>
       <Toaster />
-      <Container>
-        <Stack spacing={5}>
-          <TechForm />
-          <Divider />
-          <PromotionForm />
-          <Divider />
-          <LoginForm />
-        </Stack>
-      </Container>
+      <QueryClientProvider client={client}>
+        <Container>
+          <Stack spacing={5}>
+            <TechForm />
+            <Divider />
+            <PromotionForm />
+            <Divider />
+            <LoginForm />
+            <TechList></TechList>
+          </Stack>
+        </Container>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
--- src/hooks/useSubmitForm.tsx
+++ src/hooks/useSubmitForm.tsx
@@ -1,30 +1,36 @@
-import toast from 'react-hot-toast';
-import { IErrorResponse } from '../types';
+import toast from "react-hot-toast"
+import { IErrorResponse } from "../types"
 
 const useSubmitForm = () => {
-  const submitHandler = async (url: string, data: FormData | Record<string, unknown>) => {
-    const isFormData = data instanceof FormData;
+  const submitHandler = async (
+    url: string,
+    data: FormData | Record<string, unknown>,
+    method: "POST" | "PATCH"
+  ) => {
+    const isFormData = data instanceof FormData
 
-    const body = isFormData ? data : JSON.stringify(data);
+    const body = isFormData ? data : JSON.stringify(data)
 
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}${url}`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: !isFormData ? {
-        'Content-Type': 'application/json',
-      } : undefined,
-      body,
-    });
+      method: method,
+      credentials: "include",
+      headers: !isFormData
+        ? {
+            "Content-Type": "application/json"
+          }
+        : undefined,
+      body
+    })
 
     if (!response.ok) {
-      const res = await response.json() as IErrorResponse;
-      return toast.error(res.message);
-    } 
-    
-    toast.success('Submission successful');
-  };
+      const res = (await response.json()) as IErrorResponse
+      return toast.error(res.message)
+    }
 
-  return submitHandler;
-};
+    toast.success("Submission successful")
+  }
 
-export default useSubmitForm;
+  return submitHandler
+}
+
+export default useSubmitForm
