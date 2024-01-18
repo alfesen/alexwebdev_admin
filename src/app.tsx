abb8e7497207507fadf3dfcb871ecd794d5035e5
unknown <alfesen>
2024-01-18 12:08:29 +0100

    Create Login page + container widths

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import { Container, Divider, Stack } from '@mui/material'
 import TechForm from './components/forms/TechForm'
 import PromotionForm from './components/forms/PromotionForm'
-import LoginForm from './components/forms/LoginForm'
 import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import TechList from './components/lists/Tech/TechList'
 import PromotionList from './components/lists/Promotions/PromotionList'
 import { useEffect, useState } from 'preact/hooks'
 import Cookies from 'js-cookie'
+import LoginPage from './components/pages/LoginPage'
 
 export function App() {
   const [isAuth, setIsAuth] = useState(
@@ -33,9 +33,9 @@ export function App() {
       <Toaster />
       <QueryClientProvider client={client}>
         {!isAuth ? (
-          <LoginForm />
+          <LoginPage />
         ) : (
-          <Container>
+          <Container maxWidth='md'>
             <Stack spacing={5}>
               <TechForm />
               <Divider />
---
+++ src/components/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material"
+import LoginForm from "../forms/LoginForm"
+
+const LoginPage = () => {
+  return (
+
+    <Container maxWidth='xs'>
+      <LoginForm />
+    </Container >
+  )
+}
+
+export default LoginPage
