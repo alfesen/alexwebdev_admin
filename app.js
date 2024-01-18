0f25619486fa824c4fa5bc2131606f1befc5cf7f
unknown <alfesen>
2024-01-18 11:49:02 +0100

    Cookie check for auth

---
+++ app.js
@@ -0,0 +1,30 @@
+import {app, BrowserWindow} from 'electron'
+import path from 'path'
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:5173');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
--- src/app.tsx
+++ src/app.tsx
@@ -6,24 +6,47 @@ import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import TechList from './components/lists/Tech/TechList'
 import PromotionList from './components/lists/Promotions/PromotionList'
+import { useEffect, useState } from 'preact/hooks'
+import Cookies from 'js-cookie'
 
 export function App() {
+  const [isAuth, setIsAuth] = useState(
+    !!Cookies.get(import.meta.env.VITE_LOGIN_COOKIE)
+  )
+  console.log(Cookies.get(import.meta.env.VITE_LOGIN_COOKIE))
+  useEffect(() => {
+    // Function to check for cookie changes
+    const checkForCookieChange = () => {
+      setIsAuth(!!Cookies.get(import.meta.env.VITE_LOGIN_COOKIE))
+    }
+
+    // Set up an interval to periodically check for cookie changes
+    const intervalId = setInterval(checkForCookieChange, 1000) // Adjust the interval as needed
+
+    // Clear the interval on component unmount to avoid memory leaks
+    return () => clearInterval(intervalId)
+  }, [isAuth])
+
   const client = new QueryClient()
   return (
     <>
       <Toaster />
       <QueryClientProvider client={client}>
-        <Container>
-          <Stack spacing={5}>
-            <TechForm />
-            <Divider />
-            <PromotionForm />
-            <Divider />
-            <LoginForm />
-            <PromotionList />
-            <TechList></TechList>
-          </Stack>
-        </Container>
+        {!isAuth ? (
+          <LoginForm />
+        ) : (
+          <Container>
+            <Stack spacing={5}>
+              <TechForm />
+              <Divider />
+              <PromotionForm />
+              <Divider />
+              <PromotionList />
+              <Divider />
+              <TechList></TechList>
+            </Stack>
+          </Container>
+        )}
       </QueryClientProvider>
     </>
   )
