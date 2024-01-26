abb93bf3fe69c2d2b6a0bff1180b7032ff4e2c80
unknown <alfesen>
2024-01-26 11:44:22 +0100

    Create useRemove hook and implement in list items

---
+++ src/hooks/useRemove.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useMutation } from 'react-query'
+import { client } from '../app'
+import { Updater } from 'react-query/types/core/utils'
+
+const useRemove = ({
+  id,
+  category,
+  mutationKey,
+  queryKey,
+  options
+}: {
+  id: string
+  category: string
+  mutationKey: string[]
+  queryKey: string[]
+  options?: { objectKey: string }
+}) => {
+  const { mutate } = useMutation({
+    mutationKey: [mutationKey],
+    mutationFn: async () => {
+      try {
+        const { data } = await axios.delete(
+          `${import.meta.env.VITE_SERVER_URL}/${category}/${id}`,
+          {withCredentials: true}
+        )
+        return toast.success(data.message)
+      } catch (err) {
+        if (err instanceof Error) {
+          return toast.error(err.message)
+        }
+      }
+    },
+    onMutate: async () => {
+      await client.cancelQueries({ queryKey: queryKey })
+      const previousData = (await client.getQueryData(queryKey)) as any
+      if (options?.objectKey) {
+        const newData: Record<string, unknown[]> = {
+          ...previousData,
+          [options.objectKey]: previousData[options.objectKey].filter(
+            (t: any) => t.id !== id
+          )
+        }
+        client.setQueryData(queryKey, newData)
+        return previousData
+      }
+      client.setQueryData(queryKey, (old: Updater<any[], any>) =>
+        old.filter((m: { id: string }) => m.id !== id)
+      )
+      return previousData
+    }
+  })
+
+  return { removeItem: mutate }
+}
+
+export default useRemove
